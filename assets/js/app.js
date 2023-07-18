527db19e823eff6f7f9226f7466878d20ef3076a
Chí Hưởng <>
2023-07-19 00:36:43 +0700

    slick_selling done responsive max600

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+
+$('.content_sell').slick({
+    dots:false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows:false,
+    responsive:[
+            
+        {
+            breakpoint: 600,
+            settings: {
+            dots:true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows:false,
+        }},
+    ]
+    });
